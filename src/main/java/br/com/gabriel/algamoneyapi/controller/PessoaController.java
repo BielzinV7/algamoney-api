758b234451d77dc3979713906a8b53bf03d2f385
BielzinV7 <gabrielcosta.v7@gmail.com>
2020-10-29 21:49:38 -0300

    implementando o controller de pessoa

---
+++ src/main/java/br/com/gabriel/algamoneyapi/controller/PessoaController.java
@@ -0,0 +1,41 @@
+package br.com.gabriel.algamoneyapi.controller;
+
+import br.com.gabriel.algamoneyapi.model.Pessoa;
+import br.com.gabriel.algamoneyapi.repository.PessoaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/pessoa")
+public class PessoaController{
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    @PostMapping
+    public ResponseEntity<Pessoa> cadastrarPessoa(@Valid @RequestBody Pessoa pessoa, UriComponentsBuilder uriBuilder){
+
+        Pessoa pessoaSalva = pessoaRepository.save(pessoa);
+
+        URI uri = uriBuilder.path("/pessoa/{codigo}").buildAndExpand(pessoa.getCodigo()).toUri();
+
+        return ResponseEntity.created(uri).body(pessoaSalva);
+
+    }
+
+    @GetMapping("/{codigo}")
+    public ResponseEntity<Pessoa> buscarPeloId(@PathVariable Long codigo){
+
+        Optional<Pessoa> pessoaOptional = pessoaRepository.findById(codigo);
+
+        return pessoaOptional.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+
+
+    }
+}
