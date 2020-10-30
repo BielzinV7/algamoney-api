cd2adec2b178075c6c4dec2016391b187286c83e
BielzinV7 <gabrielcosta.v7@gmail.com>
2020-10-29 21:55:34 -0300

    Alterando as anotações do @BEAN

--- src/main/java/br/com/gabriel/algamoneyapi/model/Categoria.java
+++ src/main/java/br/com/gabriel/algamoneyapi/model/Categoria.java
@@ -1,6 +1,7 @@
 package br.com.gabriel.algamoneyapi.model;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Objects;
