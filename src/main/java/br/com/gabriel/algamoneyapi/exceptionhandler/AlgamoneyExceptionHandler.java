379d5f68e9fd795ab0a222679022e12b0473538e
BielzinV7 <gabrielcosta.v7@gmail.com>
2020-10-30 17:17:59 -0300

    Tratando de Erro pra caso o usuario tente deletar um recurso que já foi deletado

--- src/main/java/br/com/gabriel/algamoneyapi/exceptionhandler/AlgamoneyExceptionHandler.java
+++ src/main/java/br/com/gabriel/algamoneyapi/exceptionhandler/AlgamoneyExceptionHandler.java
@@ -3,6 +3,7 @@ package br.com.gabriel.algamoneyapi.exceptionhandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,8 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -32,6 +35,17 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
         String mensagemDesenvolvedor = ex.getCause().toString();
         List<Erro> erros = Arrays.asList(new Erro(mensagemUsuairo,mensagemDesenvolvedor));
         return handleExceptionInternal(ex,erros,headers,HttpStatus.BAD_REQUEST,request);
+    }
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex, WebRequest request){
+
+        String mensagemUsuairo = messageSource.getMessage("recurso.nao-encontrado",null, LocaleContextHolder.getLocale());
+        String mensagemDesenvolvedor = ex.toString();
+        List<Erro> erros = Arrays.asList(new Erro(mensagemUsuairo,mensagemDesenvolvedor));
+        return handleExceptionInternal(ex,erros,new HttpHeaders(),HttpStatus.NOT_FOUND,request);
+
     }
 
     @Override
