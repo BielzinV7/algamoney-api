c95e1ab2aa84ff434e79285da4403e7da35ef835
BielzinV7 <gabrielcosta.v7@gmail.com>
2020-10-31 10:52:45 -0300

    Passando a regra de negócio do CONTROLLER pra SERVICE

--- src/main/java/br/com/gabriel/algamoneyapi/controller/PessoaController.java
+++ src/main/java/br/com/gabriel/algamoneyapi/controller/PessoaController.java
@@ -2,6 +2,8 @@ package br.com.gabriel.algamoneyapi.controller;
 
 import br.com.gabriel.algamoneyapi.model.Pessoa;
 import br.com.gabriel.algamoneyapi.repository.PessoaRepository;
+import br.com.gabriel.algamoneyapi.service.PessoaService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +20,9 @@ public class PessoaController{
 
     @Autowired
     private PessoaRepository pessoaRepository;
+
+    @Autowired
+    private PessoaService pessoaService;
 
     @PostMapping
     public ResponseEntity<Pessoa> cadastrarPessoa(@Valid @RequestBody Pessoa pessoa, UriComponentsBuilder uriBuilder){
@@ -45,5 +50,21 @@ public class PessoaController{
 
         pessoaRepository.deleteById(codigo);
     }
+
+    @PutMapping("/{codigo}")
+    public ResponseEntity<Pessoa> atualizar(@Valid @RequestBody Pessoa pessoa,@PathVariable Long codigo){
+
+        Pessoa pessoaSalva = pessoaService.atualizar(codigo,pessoa);
+        return ResponseEntity.ok(pessoaSalva);
+    }
+
+    @PutMapping("/{codigo}/ativo")
+    public void atualizarPropiedadeAtivo(@PathVariable Long codigo,@RequestBody Boolean ativo){
+
+        pessoaService.atualizarPropriedadeAtivo(codigo,ativo);
+
+    }
+
+
 
 }
