59a18fe561d41a541a47548d508e63ec052357f2
BielzinV7 <gabrielcosta.v7@gmail.com>
2020-10-31 10:51:05 -0300

    implementando a Service de pessoa

---
+++ src/main/java/br/com/gabriel/algamoneyapi/service/PessoaService.java
@@ -0,0 +1,38 @@
+package br.com.gabriel.algamoneyapi.service;
+
+import br.com.gabriel.algamoneyapi.model.Pessoa;
+import br.com.gabriel.algamoneyapi.repository.PessoaRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PessoaService {
+
+    @Autowired
+    PessoaRepository pessoaRepository;
+
+    public Pessoa atualizar(Long codigo,Pessoa pessoa){
+
+        Pessoa pessoaSalva = buscarPessoa(codigo);
+        BeanUtils.copyProperties(pessoa,pessoaSalva,"codigo");
+        return pessoaRepository.save(pessoaSalva);
+
+    }
+
+    public Pessoa atualizarPropriedadeAtivo(Long codigo, Boolean ativo) {
+        Pessoa pessoaSalva = buscarPessoa(codigo);
+        pessoaSalva.setAtivo(ativo);
+        return pessoaRepository.save(pessoaSalva);
+
+    }
+
+    //metodo de buscar pessoa pra ser reaproveitado nas regras de negócio
+    private Pessoa buscarPessoa(Long codigo){
+
+        Pessoa pessoaSalva = pessoaRepository.findById(codigo).orElseThrow(() -> new EmptyResultDataAccessException(1));
+        return pessoaSalva;
+
+    }
+}
