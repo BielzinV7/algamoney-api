8310b6ceb138487e2acb5ca04372f90253c838ac
BielzinV7 <gabrielcosta.v7@gmail.com>
2020-11-21 13:30:55 -0300

    Extendendo o LancamentoRepositoryQuery

--- src/main/java/br/com/gabriel/algamoneyapi/repository/LancamentoRepository.java
+++ src/main/java/br/com/gabriel/algamoneyapi/repository/LancamentoRepository.java
@@ -1,8 +1,13 @@
 package br.com.gabriel.algamoneyapi.repository;
 
 import br.com.gabriel.algamoneyapi.model.Lancamento;
+import br.com.gabriel.algamoneyapi.repository.filter.LancamentoFilter;
+import br.com.gabriel.algamoneyapi.repository.lancamento.LancamentoRepositoryQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LancamentoRepository extends JpaRepository<Lancamento,Long> {
+import java.util.List;
+
+public interface LancamentoRepository extends JpaRepository<Lancamento,Long> , LancamentoRepositoryQuery{
+
 
 }
